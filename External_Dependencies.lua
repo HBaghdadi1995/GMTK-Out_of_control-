4b4114c8be22f5e1d1f13caec84616da21c49f08
HBaghdadi1995 <34271143+HBaghdadi1995@users.noreply.github.com>
2020-07-10 23:51:11 +0100

    remove glew

--- External_Dependencies.lua
+++ External_Dependencies.lua
@@ -3,3 +3,8 @@ function importGLFW()
     includedirs {"../../External/glfw/include"}
     links{"glfw"}
 end
+
+function importGLAD()
+    includedirs {"../../External/glad/include"}
+    links{"glad"}
+end

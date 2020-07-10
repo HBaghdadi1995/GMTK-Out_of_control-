dd6f721540ae1583565f2b0f54e258cb2827527d
HBaghdadi1995 <34271143+HBaghdadi1995@users.noreply.github.com>
2020-07-11 00:38:11 +0100

    Triangle done

--- Core/Application/src/main.cpp
+++ Core/Application/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "glad/glad.h"
+
+#include <glad/glad.h>
 #include "Window.h"
 
 int main(int argc, char *argv[]){
@@ -13,13 +14,39 @@ int main(int argc, char *argv[]){
         assert(false);
     }
 
+    GLuint buffer;
+
+    float positions[6] =
+    { 0.0f, 0.5f,
+      -0.5f, -0.5f,
+      0.5f, -0.5f };
+
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), &positions, GL_STATIC_DRAW);
+
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        2,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        sizeof(float),                  // stride
+        0            // array buffer offset
+    );
+
     if (!gladLoadGL()) {
         assert(false);
     }
+    glPointSize(32);
 
     while (Window::Instance()->Update())
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glViewport(0, 0, 640, 480);
+        glDrawArrays(GL_POINTS, 0, 3);
     }
 
     glfwTerminate();
